63d5050acbd1c4d178ba0138130b49c111661bdf
Paul Roeland <paula@polyester.org>
2014-03-29 15:55:40 +0100

    fix getting externals

--- get_external_doc.sh
+++ get_external_doc.sh
@@ -5,11 +5,13 @@ do
     if [ -d "$path" ]; then
         cd $path
         git pull
+        cd -
     else
         git clone $source $path
         cd $path
         git config core.sparsecheckout true
         echo docs/ > .git/info/sparse-checkout
         git read-tree -m -u HEAD
+        cd -
     fi
 done < externals.txt
